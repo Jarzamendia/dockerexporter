2cb351fe0ad78889eb65585f9210b8ed6b0de3d7
Joao E. Arzamendia <jearzamendia@gmail.com>
2019-05-16 19:01:35 -0400

    initial deploy

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+	"golang.org/x/net/context"
+)
+
+func main() {
+	cli, err := client.NewEnvClient()
+	if err != nil {
+		panic(err)
+	}
+
+	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	for _, container := range containers {
+		fmt.Println(container.ID)
+    }
+    
+    images, err := cli.ImageList(context.Background(), types.ImageListOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	for _, image := range images {
+		fmt.Println(image.ID)
+    }
+    
+    services, err := cli.ServiceList(context.Background())
+	if err != nil {
+		panic(err)
+    }
+    
+    for _, service := range services {
+		fmt.Println(service.ID)
+    }
+    
+
+}
